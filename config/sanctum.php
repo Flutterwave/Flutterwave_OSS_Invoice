bbb3c785950150067c9381acc7ef19d3d7fcad44
Salvation Arinze <modernbabbage@gmail.com>
2020-09-15 09:45:24 +0100

    Prod build

--- config/sanctum.php
+++ config/sanctum.php
@@ -14,7 +14,7 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS',
-      'localhost,gotodispatch.xyz,app.gotodispatch.xyz,')),
+      'localhost,gotodispatch.xyz,app.gotodispatch.xyz,www.gotodispatch.xyz')),
 
     /*
     |--------------------------------------------------------------------------
