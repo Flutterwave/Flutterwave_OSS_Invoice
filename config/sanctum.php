2ec899c0c4c811c66354b6cf9263f8d144f0bbdf
Salvation Arinze <modernbabbage@gmail.com>
2020-09-15 23:00:13 +0100

    Added Forgot Password

--- config/sanctum.php
+++ config/sanctum.php
@@ -14,7 +14,7 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS',
-      'localhost,www.gotodispatch.xyz,gotodispatch.xyz,app.gotodispatch.xyz')),
+      'localhost,www.gotodispatch.xyz,gotodispatch.xyz')),
 
     /*
     |--------------------------------------------------------------------------
